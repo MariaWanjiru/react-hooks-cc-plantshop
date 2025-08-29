6f794913caf803e67824892f6f00c42a0d86d2eb
mariawanjiru <rushi.mariah@gmail.com>
2025-08-29 20:12:52 +0300

    react-hooks-cc-plantshop

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      <Link to="/" style={{ margin: "0 10px" }}>Plants</Link>
+      <Link to="/home" style={{ margin: "0 10px" }}>Home</Link>
+      <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
+      <Link to="/users" style={{ margin: "0 10px" }}>Users</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
